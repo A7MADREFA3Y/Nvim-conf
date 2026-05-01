1ba023eb1da6b6023a990813fe7cc5589d9afe72
a7madrefa3y <ahmadrefai2@gmail.com>
2026-05-01 16:27:55 +0200

    add stay-centered plugens

---
+++ lua/plugins/stay-centered.lua
@@ -0,0 +1,20 @@
+-- https://github.com/arnamak/stay-centered.nvim
+return {
+  {
+    "arnamak/stay-centered.nvim",
+    enabled = vim.g.scrollback_mode ~= "neobean" and vim.g.simpler_scrollback ~= "deeznuts",
+
+    require("stay-centered").setup({
+      -- The filetype is determined by the vim filetype, not the file extension. In order to get the filetype, open a file and run the command:
+      -- :lua print(vim.bo.filetype)
+      skip_filetypes = {},
+      -- Set to false to disable by default
+      enabled = true,
+      -- allows scrolling to move the cursor without centering, default recommended
+      allow_scroll_move = true,
+      -- temporarily disables plugin on left-mouse down, allows natural mouse selection
+      -- try disabling if plugin causes lag, function uses vim.on_key
+      disable_on_mouse = true,
+    }),
+  },
+}
