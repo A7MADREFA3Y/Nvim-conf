f38d60ccf98711e23cf9fa6155f2e19f06a14c0a
a7madrefa3y <ahmadrefai2@gmail.com>
2026-05-02 00:14:24 +0200

    added delete to void keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "<Up>", "<nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "<Down>", "<nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "<Left>", "<nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "<Right>", "<nop>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "dd", '"_dd', { desc = "Delete line to void" })
+vim.keymap.set("v", "d", '"_d', { desc = "Delete selection to void" })
