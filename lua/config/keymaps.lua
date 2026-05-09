f9903c0f01baddad89dc82823c4ddefbb73ef045
a7madrefa3y <ahmadrefai2@gmail.com>
2026-05-09 18:10:48 +0200

    new stuff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,3 +20,8 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+
+
+vim.keymap.set("n", "<leader>bo", function() 
+  Snacks.bufdelete.other()
+end, { desc = "Delete Other Buffers" })
