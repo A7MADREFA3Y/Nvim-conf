ad52a9585ba316fbee3aed884c07356f99fb6879
a7madrefa3y <ahmadrefai2@gmail.com>
2026-05-09 20:34:55 +0200

    update

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,29 +1,29 @@
 -- lua/plugins/bufferline.lua
 
 return {
-  "akinsho/bufferline.nvim",
-  event = "BufReadPost",
-  keys = {
-    { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next buffer" },
-    { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev buffer" },
-  },
-  opts = {
-    options = {
-      diagnostics = "nvim_lsp",
-      offsets = {
-        {
-          filetype = "neo-tree",
-          text = "File Explorer",
-          text_align = "center",
-          separator = true,
-        },
-      },
-    },
-  },
-  init = function()  -- Offset-Patch hier
-    local Offset = require("bufferline.offset")
-    if not Offset.edgy then
-      -- ... gleicher Patch-Code wie oben ...
-    end
-  end,
+	"akinsho/bufferline.nvim",
+	event = "BufReadPost",
+	keys = {
+		{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next buffer" },
+		{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev buffer" },
+	},
+	opts = {
+		options = {
+			diagnostics = "nvim_lsp",
+			offsets = {
+				{
+					filetype = "neo-tree",
+					text = "File Explorer",
+					text_align = "center",
+					separator = true,
+				},
+			},
+		},
+	},
+	init = function() -- Offset-Patch hier
+		local Offset = require("bufferline.offset")
+		if not Offset.edgy then
+			-- ... gleicher Patch-Code wie oben ...
+		end
+	end,
 }
