3d823cd23368cc92d08b4f60869c91a47de243be
a7madrefa3y <ahmadrefai2@gmail.com>
2026-05-10 09:25:04 +0200

    configuire key maps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,3 +15,20 @@ vim.keymap.set("n", "<s-j>", "<nope>", { noremap = true, silent = true })
 -- Swap them: Leader - for vertical, Leader | for horizontal
 vim.keymap.set("n", "<leader>-", ":vsplit<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>|", ":split<CR>", { noremap = true, silent = true })
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+
+-- delete old maping
+vim.keymap.del("n", "<leader>K")
+vim.keymap.del("n", "<leader>L")
+vim.keymap.del("n", "<leader>S")
+vim.keymap.del("n", "<leader>.")
+vim.keymap.del("n", "<leader>bl")
+vim.keymap.del("n", "<leader>br")
+vim.keymap.del("n", "<leader>bj")
+vim.keymap.del("n", "<leader>cA")
+vim.keymap.del("n", "<leader>cc")
+vim.keymap.del("n", "<leader>cC")
