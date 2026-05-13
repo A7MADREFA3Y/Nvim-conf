a481925d664cd50c7f18fa77a8bfcf5d242fc73b
a7madrefa3y <ahmadrefai2@gmail.com>
2026-05-13 22:02:57 +0200

    flash and tranparent theme

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -33,6 +33,8 @@
 
 return {
 	"folke/flash.nvim",
+	event = "VeryLazy",
+	vscode = true,
 	opts = {
 		-- I don't want aiorx as options when in flash, not sure why, but I don't
 		-- labels = "asdfghjklqwertyuiopzxcvbnm",
@@ -61,6 +63,62 @@ return {
 				-- the next `f`s
 				enabled = false,
 			},
+		},
+	},
+	keys = {
+		{
+			"s",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"S",
+			mode = { "n", "o", "x" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
+		},
+		{
+			"r",
+			mode = "o",
+			function()
+				require("flash").remote()
+			end,
+			desc = "Remote Flash",
+		},
+		{
+			"R",
+			mode = { "o", "x" },
+			function()
+				require("flash").treesitter_search()
+			end,
+			desc = "Treesitter Search",
+		},
+		{
+			"<c-s>",
+			mode = { "c" },
+			function()
+				require("flash").toggle()
+			end,
+			desc = "Toggle Flash Search",
+		},
+		-- Simulate nvim-treesitter incremental selection
+		{
+			"<c-space>",
+			mode = { "n", "o", "x" },
+			function()
+				require("flash").treesitter({
+					actions = {
+						["<c-space>"] = "next",
+						["<BS>"] = "prev",
+					},
+				})
+			end,
+			desc = "Treesitter Incremental Selection",
 		},
 	},
 }
