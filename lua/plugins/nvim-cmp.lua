08ccd80e69accf2037365dc967d538cc3e83f389
a7madrefa3y <ahmadrefai2@gmail.com>
2026-05-19 22:30:08 +0200

    keymaps of cmo

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -56,16 +56,48 @@ return {
 				}),
 			},
 
-			mapping = cmp.mapping.preset.insert({
-				["<A-k>"] = cmp.mapping.select_prev_item(),
-				["<A-j>"] = cmp.mapping.select_next_item(),
-				["<A-b>"] = cmp.mapping.scroll_docs(-4),
-				["<A-f>"] = cmp.mapping.scroll_docs(4),
-				["<A-Space>"] = cmp.mapping.complete(),
-				["<A-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-			}),
+			-- mapping = cmp.mapping.preset.insert({
+			-- 	["<A-k>"] = cmp.mapping.select_prev_item(),
+			-- 	["<A-j>"] = cmp.mapping.select_next_item(),
+			-- 	["<A-b>"] = cmp.mapping.scroll_docs(-4),
+			-- 	["<A-f>"] = cmp.mapping.scroll_docs(4),
+			-- 	["<A-Space>"] = cmp.mapping.complete(),
+			-- 	["<A-e>"] = cmp.mapping.abort(),
+			-- 	["<CR>"] = cmp.mapping.confirm({ select = false }),
+			-- }),
+       mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
 
+    ["<CR>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    },
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif require("luasnip").expand_or_jumpable() then
+        require("luasnip").expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif require("luasnip").jumpable(-1) then
+        require("luasnip").jump(-1)
+      else
+        fallback()
+      end
+            end, { "i", "s" }),
+  },
 			sources = {
 				{ name = "codeium" },
 				{ name = "luasnip" },
